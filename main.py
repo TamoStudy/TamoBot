1e526453b24b19c33f03f8aec2ced824804ca5f3
Anthony <59890724+narlock@users.noreply.github.com>
2023-04-04 17:05:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord import app_commands
 from tamo_secrets import TamoSecrets
 
 # set up the bot client with intents
-bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 allowed_server = int(TamoSecrets.get_server())
 
 @bot.event
@@ -21,7 +21,7 @@ async def on_ready():
             print(f'- {guild.name} (id: {guild.id})')
 
     # Set status message
-    await bot.change_presence(activity=discord.Game(name="$help | narlock.dev"))
+    await bot.change_presence(activity=discord.Game(name="/help | tamostudy.com"))
 
     # Sync commands
     synced = await bot.tree.sync()
@@ -47,43 +47,60 @@ async def hello(interaction: discord.Interaction):
     # send a message to the user who entered the command
     await interaction.response.send_message(content='Hello!')
     
-@bot.command(name='roll')
-async def roll(ctx, max_roll: str = '100'):
+@bot.tree.command(name='roll', description='Rolls a random number between 1 and a max number (default 100)')
+async def roll(interaction: discord.Interaction, max_roll: str = '100'):
     """
     Rolls a random number between 1 and a maximum number (default 100)
     Usage: !roll [max_roll]
     """
+    footer_message = None
+    roll_message = 'Unexpected error occurred during /roll command.'
     try:
         max_roll = int(max_roll)
     except ValueError:
-        await ctx.send("The maximum roll value must be an integer.")
+        roll_message = 'The maximum roll value must be an integer.'
+        footer_message = 'Use the /roll command to try again.'
         return
 
     if max_roll < 1:
-        await ctx.send("The maximum roll value must be greater than or equal to 1.")
+        roll_message = 'The maximum roll value must be greater than or equal to 1.'
+        footer_message = 'Use the /roll command to try again.'
     elif max_roll > 99999:
-        await ctx.send("The maximum roll value cannot exceed 99999.")
+        roll_message = 'The maximum roll value cannot exceed 99999.'
+        footer_message = 'Use the /roll command to try again.'
     else:
         roll_number = random.randint(1, max_roll)
-        await ctx.send(f"{ctx.author.mention} rolled a {roll_number} out of {max_roll}!")
+        roll_message = f"{interaction.user.name} rolled a {roll_number} out of {max_roll}!"
 
-@bot.command(name='rules')
-async def rules(ctx):
+    embed = discord.Embed(title=f'{roll_message}', color=0xffa500)
+    embed.set_thumbnail(url=f'{interaction.user.avatar.url}')
+    if footer_message is not None:
+        embed.add_field(name='\u200b', value=footer_message)
+    embed.add_field(name='\u200b', value='Powered by [**narlock.dev**](https://narlock.github.io/narlock)', inline=False)
+
+    await interaction.response.send_message(embed=embed)
+
+@bot.tree.command(name='rules', description='Displays the rules for the server.')
+async def rules(interaction: discord.Interaction):
     """
     Displays the rules for the server.
     """
-    embed = Embed(title='Server Rules')
-    embed.add_field(name='Rule #1', value='Always follow [Discord\'s official Terms of Service](https://discord.com/terms)', inline = False)
-    embed.add_field(name='Rule #2', value='Be respectful to other users; any form of discrimination will not be tolerated. This includes any conversation dealing with politics or religion - it will not be tolerated.', inline = False)
-    embed.add_field(name='Rule #3', value='Initiating in fights/controversial discussion will not be tolerated.', inline = False)
-    embed.add_field(name='Rule #4', value='Do not send harmful material such as viruses, IP-grabbers, or harm-ware.', inline = False)
-    embed.add_field(name='Rule #5', value='Advertising of other servers/websites will not be tolerated.', inline = False)
-    embed.add_field(name='Rule #6', value='Use common sense. If you think it\'s going to get you in trouble, odds are it will.', inline = False)
-    embed.add_field(name='Rule #7', value='English only in text channels. You may speak in other languages in <#1030510653003276318>.', inline = False)
-    embed.add_field(name='Rule #8', value='Impersonating other users will not be tolerated.', inline = False)
-    embed.set_footer(text='Thanks for following the rules!')
+    rules_channel = bot.get_channel(821757961830793239)
 
-    await ctx.send(embed=embed)
+    embed = discord.Embed(title='Server Rules', color=0xff3a40)
+    embed.set_thumbnail(url='https://github.com/narlock/Kaizen/blob/main/KaizenClient/assets/INFO_ERROR_ORANGE.png?raw=true')
+    embed.add_field(name='\u200b', value='**1** → Always follow [Discord\'s official Terms of Service](https://discord.com/terms)', inline = False)
+    embed.add_field(name='\u200b', value='**2** → Be respectful to other users; any form of discrimination will not be tolerated.', inline = False)
+    embed.add_field(name='\u200b', value='**3** → Initiating in fights/controversial discussion will not be tolerated.', inline = False)
+    embed.add_field(name='\u200b', value='**4** → Do not send harmful material such as viruses, IP-grabbers, or harm-ware.', inline = False)
+    embed.add_field(name='\u200b', value='**5** → Advertising of other servers/websites will not be tolerated.', inline = False)
+    embed.add_field(name='\u200b', value='**6** → Use common sense. If you think it\'s going to get you in trouble, odds are it will.', inline = False)
+    embed.add_field(name='\u200b', value='**7** → English only in text channels. You may speak in other languages in <#1030510653003276318>.', inline = False)
+    embed.add_field(name='\u200b', value='**8** → Impersonating other users will not be tolerated.', inline = False)
+    embed.add_field(name='\u200b', value=f'**Refer to {rules_channel.mention} for more information**.', inline = False)
+
+    await interaction.response.send_message(embed=embed)
+
 
 # Start the bot
 print(TamoSecrets.get_server())
