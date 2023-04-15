bcf5019e1da47ce800e1895f8af09866eff04836
Anthony <59890724+narlock@users.noreply.github.com>
2023-04-15 10:03:55 -0500

    update rules.py

--- apps/arcade/arcade.py
+++ apps/arcade/arcade.py
@@ -5,7 +5,7 @@ class Arcade():
     def show_arcade_options():
         embed = discord.Embed(title="TamoBot Arcade")
         embed.set_thumbnail(url='https://raw.githubusercontent.com/TamoStudy/TamoBot/main/README%20Assets/TamoBot.png')
-        embed.add_field(name=':joystick: Single Player', value='**Trivia:** answer fun and simple trivia questions with `/trivia`. (100 Tamo tokens)', inline=False)
+        embed.add_field(name=':joystick: Single Player', value='**Trivia:** answer fun and simple trivia questions\n→ `/trivia` (100 Tamo tokens)', inline=False)
         embed.add_field(name=':signal_strength: Multi Player', value='**Coming Soon**', inline=False)
         embed.add_field(name='\u200b', value='Powered by [**narlock.dev**](https://narlock.github.io/narlock)', inline=False)
         return embed
--- apps/info/rules.py
+++ apps/info/rules.py
@@ -10,14 +10,15 @@ class Rules:
 
         embed = discord.Embed(title='Server Rules', color=0xff3a40)
         embed.set_thumbnail(url='https://avatars.githubusercontent.com/u/59890724?v=4')
-        embed.add_field(name='\u200b', value='**1** → Always follow [Discord\'s official Terms of Service](https://discord.com/terms)', inline = False)
-        embed.add_field(name='\u200b', value='**2** → Be respectful to other users; any form of discrimination will not be tolerated.', inline = False)
-        embed.add_field(name='\u200b', value='**3** → Initiating in fights/controversial discussion will not be tolerated.', inline = False)
-        embed.add_field(name='\u200b', value='**4** → Do not send harmful material such as viruses, IP-grabbers, or harm-ware.', inline = False)
-        embed.add_field(name='\u200b', value='**5** → Advertising of other servers/websites will not be tolerated.', inline = False)
-        embed.add_field(name='\u200b', value='**6** → Use common sense. If you think it\'s going to get you in trouble, odds are it will.', inline = False)
-        embed.add_field(name='\u200b', value='**7** → English only in text channels. You may speak in other languages in <#1030510653003276318>.', inline = False)
-        embed.add_field(name='\u200b', value='**8** → Impersonating other users will not be tolerated.', inline = False)
+        embed.add_field(name='\u200b', value='**1** → Always follow [Discord\'s official Terms of Service](https://discord.com/terms)'
+                        + '\n**2** → Be respectful to other users; any form of discrimination will not be tolerated.'
+                        + '\n**3** → Initiating in fights/controversial discussion will not be tolerated.'
+                        + '\n**4** → Do not send harmful material such as viruses, IP-grabbers, or harm-ware.'
+                        + '\n**5** → Advertising of other servers/websites will not be tolerated.'
+                        + '\n**6** → Use common sense. If you think it\'s going to get you in trouble, odds are it will.'
+                        + '\n**7** → English only in text channels. You may speak in other languages in <#1030510653003276318>.'
+                        + '\n**8** → Impersonating other users will not be tolerated.'
+                        , inline = False)
         embed.add_field(name='\u200b', value=f'**Refer to {rules_channel.mention} for more information**.', inline = False)
 
         return embed
